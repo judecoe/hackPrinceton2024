0a72098abb643d202b027c4cc86771f275a2f3b3
judecoe <judegilligan@gmail.com>
2024-11-08 23:34:38 -0500

    Co-authored-by: MichaelCai2005 <MichaelCai2005@users.noreply.github.com> Co-authored-by: kiermurr <kiermurr@users.noreply.github.com> Co-authored-by: Ilya Abbasian <Ilya-Abbasian@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic21pdGhidW5rcyIsImEiOiJjbTM5bDZrZG4xN2E5MmlwdGxqbXA2ZXc3In0.Y2O5J7Dl_8ViBPH2BZblNg';
 
 function App() {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v9', 
+      center: [-74.6551, 40.3431], 
+      zoom: 14, 
+      //test
+    });
+
+    return () => map.remove();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh' }}>
+      <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />
     </div>
   );
 }
