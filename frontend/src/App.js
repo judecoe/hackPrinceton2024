3cea8901ff8cb44af1eb6b27eaed84ce9e828721
kiermurr <KFM41@pitt.edu>
2024-11-09 20:30:43 -0500

    firebase properly implemented into frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,24 @@ import './App.css';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {accessToken} from './env';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {   // lowercase 'f'
+  apiKey: "AIzaSyDjs_g9yeydqqWLjdfrecTMpoPEKJRTTvo",
+  authDomain: "college-surfing.firebaseapp.com",
+  projectId: "college-surfing",
+  storageBucket: "college-surfing.firebasestorage.app",
+  messagingSenderId: "486454469198",
+  appId: "1:486454469198:web:c05f409c0f0e349415d91f",
+  measurementId: "G-ZFWRK1JRS9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);  // lowercase 'f'
+const db = getFirestore(app);
+
+export { db };
 
 mapboxgl.accessToken = accessToken;
 
@@ -12,9 +30,9 @@ function App() {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v9', 
-      center: [-74.6551, 40.3431], 
-      zoom: 14, 
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [-74.6551, 40.3431],
+      zoom: 14,
     });
 
     return () => map.remove();
