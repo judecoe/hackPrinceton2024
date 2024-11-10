d8da2d52c1f7ccb008071ae62062e378318dfee4
kiermurr <KFM41@pitt.edu>
2024-11-09 20:39:50 -0500

    small change ya feel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from './Map';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { db } from './firebase';
 
 const firebaseConfig = {   // lowercase 'f'
   apiKey: "AIzaSyDjs_g9yeydqqWLjdfrecTMpoPEKJRTTvo",
